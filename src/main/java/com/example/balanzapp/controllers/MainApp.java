b07818de6ddbf8aca26ee64c07fa4d806694444b
DianaEspinal <dianicee2002@gmail.com>
2025-10-08 22:36:09 -0600

    Creando codigo del main

---
+++ src/main/java/com/example/balanzapp/controllers/MainApp.java
@@ -0,0 +1,33 @@
+package com.example.balanzapp.controllers;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    private static Stage primaryStage; // Referencia global al Stage
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        primaryStage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("BalanzApp");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void setRoot(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/views/" + fxml + ".fxml"));
+        Scene newScene = new Scene(fxmlLoader.load());
+        primaryStage.setScene(newScene);
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
