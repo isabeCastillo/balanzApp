286593f1ca69e6c54591fbd86836880d94a5bf1f
AngelicaPerez12 <ap22010>
2025-10-09 13:49:43 -0600

    Agregando vista de inicio y de documentos

---
+++ src/main/java/com/example/balanzapp/controllers/DocumentosController.java
@@ -0,0 +1,143 @@
+package com.example.balanzapp.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class DocumentosController {
+    @FXML
+    private Button btnagregar;
+
+    @FXML
+    private Button btnbalances;
+
+    @FXML
+    private Button btnbitacora;
+
+    @FXML
+    private Button btncatalogo;
+
+    @FXML
+    private Button btncerrar;
+
+    @FXML
+    private Button btndoc;
+
+    @FXML
+    private Button btneditar;
+
+    @FXML
+    private Button btneliminar;
+
+    @FXML
+    private Button btnestadoderesultados;
+
+    @FXML
+    private Button btninicio;
+
+    @FXML
+    private Button btnlibrodiario;
+
+    @FXML
+    private Button btnlibromayor;
+
+    @FXML
+    private Button btnusuario;
+
+    @FXML
+    private ComboBox<?> cmbElegirDoc;
+
+    @FXML
+    private Label lblUs;
+
+    @FXML
+    private Label lblad;
+
+    @FXML
+    private TextField txtNombreDocumento;
+
+    @FXML
+    private void initialize(){
+
+    }
+
+    @FXML
+    void AggDoc(ActionEvent event) {
+
+
+    }
+
+    @FXML
+    void Close(ActionEvent event) {
+
+    }
+
+    @FXML
+    void EditDoc(ActionEvent event) {
+
+    }
+
+    @FXML
+    void EliminarDoc(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToBitacoraAuditor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToBlances(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToCatalogoCuentas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToDoc(ActionEvent event) {
+        try {
+            MainApp.setRoot("documentos");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void goToEstadoResultados(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToHome(ActionEvent event) {
+        try {
+            MainApp.setRoot("inicio");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void goToLibroDiario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToLibroMayor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToUsuario(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/example/balanzapp/controllers/InicioController.java
@@ -0,0 +1,98 @@
+package com.example.balanzapp.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class InicioController {
+    @FXML
+    private Button btnbalances;
+
+    @FXML
+    private Button btnbitacora;
+
+    @FXML
+    private Button btncatalogo;
+
+    @FXML
+    private Button btncerrar;
+
+    @FXML
+    private Button btndoc;
+
+    @FXML
+    private Button btnestadoderesultados;
+
+    @FXML
+    private Button btninicio;
+
+    @FXML
+    private Button btnlibrodiario;
+
+    @FXML
+    private Button btnlibromayor;
+
+    @FXML
+    private Button btnusuario;
+
+    @FXML
+    private Label lblUs;
+
+    @FXML
+    private Label lblad;
+
+    @FXML
+    private void initialize(){
+
+    }
+
+    public void goToHome(ActionEvent actionEvent) throws IOException {
+        try {
+            MainApp.setRoot("inicio");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void goToDoc(ActionEvent actionEvent) throws IOException {
+        try {
+            MainApp.setRoot("documentos");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void goToLibroDiario(ActionEvent actionEvent) {
+    }
+
+    public void goToLibroMayor(ActionEvent actionEvent) {
+    }
+
+    public void goToBlances(ActionEvent actionEvent) {
+    }
+
+    public void goToEstadoResultados(ActionEvent actionEvent) {
+    }
+
+    public void goToCatalogoCuentas(ActionEvent actionEvent) {
+    }
+
+    public void goToUsuario(ActionEvent actionEvent) {
+    }
+
+    public void goToBitacoraAuditor(ActionEvent actionEvent) {
+    }
+
+    public void Close(ActionEvent actionEvent) {
+
+    }
+}
