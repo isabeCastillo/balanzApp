73332b48ff4102f49fd878436409e3681b565b69
isabeCastillo <cp21026@ues.edu.sv>
2025-10-08 19:24:41 -0600

    subiendo la conexion a la base de datos, y los logos con el fondo

---
+++ src/main/java/com/example/balanzapp/Conexion/ConexionDB.java
@@ -0,0 +1,25 @@
+package com.example.balanzapp.Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionDB {
+    private static String url = "jdbc:postgresql://localhost:5432/db_contable";
+
+    private static  final String USER = "postgres";
+
+    private static final String PASS = "progra2023";
+
+    //crear objeto de tipo conect
+    public static Connection connection()
+    {
+        try {
+            Connection conectar = DriverManager.getConnection(url, USER, PASS);
+            System.out.println("Conectado a la base");
+            return conectar;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
