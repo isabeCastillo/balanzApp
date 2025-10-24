119f97983cb466c1ff5544df64fd07278f161a42
DianaEspinal <dianicee2002@gmail.com>
2025-10-24 17:52:11 -0600

    Agregando estado de resultados y el controllers

---
+++ src/main/java/com/example/balanzapp/controllers/EstadosResultadosControllers.java
@@ -0,0 +1,79 @@
+package com.example.balanzapp.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class EstadosResultadosControllers {
+
+    @FXML private ComboBox<String> cmbbalances;
+    @FXML private ComboBox<String> cmbAnio;
+    @FXML private ComboBox<String> cmbMes;
+    @FXML private ComboBox<String> cmbPeriodo;
+    @FXML private Button btninicio;
+    @FXML private Button btndoc;
+    @FXML private Button btnlibrodiario;
+    @FXML private Button btnlibromayor;
+    @FXML private Button btnestadoderesultados;
+    @FXML private Button btncatalogo;
+    @FXML private Button btnusuario;
+    @FXML private Button btnbitacora;
+    @FXML private Button btncerrar;
+    @FXML private Label lblUs;
+    @FXML private Label lblad;
+    @FXML private TableView<?> tblResultados;
+
+    public void setUsuario(String usuario, String rol) {
+        lblUs.setText(usuario);
+        lblad.setText(rol);
+    }
+
+    @FXML
+    public void initialize() {
+        cmbPeriodo.getItems().addAll("Meensual", "Trimestral", "Anual");
+        cmbAnio.getItems().addAll("2023", "2024", "2025");
+        cmbMes.getItems().addAll("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+        cmbbalances.getItems().addAll("General", "Comparativo", "Consolidado");
+    }
+
+
+    @FXML
+    void Close(ActionEvent event) {
+        cambiarVista("/views/login.fxml", event);
+    }
+
+    @FXML void goToHome(ActionEvent e) { cambiarVista("/views/inicio.fxml", e); }
+    @FXML void goToDoc(ActionEvent e) { cambiarVista("/views/documentos.fxml", e); }
+    @FXML void goToLibroDiario(ActionEvent e) { cambiarVista("/views/libroDiario.fxml", e); }
+    @FXML void goToLibroMayor(ActionEvent e) { cambiarVista("/views/libroMayor.fxml", e); }
+    @FXML void goToCatalogoCuentas(ActionEvent e) { cambiarVista("/views/catalogo_cuenta.fxml", e); }
+    @FXML void goToUsuario(ActionEvent e) { cambiarVista("/views/usuarios.fxml", e); }
+    @FXML void goToBitacoraAuditor(ActionEvent e) { cambiarVista("/views/bitacora.fxml", e); }
+    @FXML void goToEstadoResultados(ActionEvent e) { cambiarVista("/views/estadosResultados.fxml", e); }
+
+
+    private void cambiarVista(String fxml, ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
+            Parent root = loader.load();
+
+            Object controller = loader.getController();
+            if (controller instanceof EstadosResultadosControllers next) {
+                next.setUsuario(lblUs.getText(), lblad.getText());
+            }
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/example/balanzapp/controllers/InicioController.java
+++ src/main/java/com/example/balanzapp/controllers/InicioController.java
@@ -164,7 +164,7 @@ public class InicioController {
 
     public void goToEstadoResultados(ActionEvent actionEvent) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("/views/estadoResultados.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/views/estadosResultados.fxml"));
             Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root));
             stage.show();
